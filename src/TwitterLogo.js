af0987f6e5a4f9606c0d80fc9f6a522732be7c8a
reatex <reatexpk1@gmail.com>
2018-06-16 18:33:28 +0500

    Improved export

--- src/TwitterLogo.js
+++ src/TwitterLogo.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-const TwitterLogo = styled.img`
+export default styled.img`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 `;
-
-export default TwitterLogo;
