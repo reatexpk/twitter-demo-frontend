3c5a29918f51a96e5249d11abea7a8628dd9ce16
reatex <reatexpk1@gmail.com>
2018-06-16 17:38:13 +0500

    Deleted unnecessary symbols and changed naming for icons

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import HomeIcon from "./img/Icon_Home.svg";
-import MomentsIcon from "./img/Icon_Moments.svg";
-import NotificationsIcon from "./img/Icon_Notifications.svg";
-import MessagesIcon from "./img/Icon_Messages.svg";
+import homeIcon from "./img/Icon_Home.svg";
+import momentsIcon from "./img/Icon_Moments.svg";
+import notificationsIcon from "./img/Icon_Notifications.svg";
+import messagesIcon from "./img/Icon_Messages.svg";
 
-const Nav = styled.nav`
-`;
+const Nav = styled.nav``;
+
 const Menu = styled.ul`
   padding-left: 0px;
   margin: 0px;
@@ -32,7 +32,6 @@ const Icon = styled.img`
   width: 19px;
   height: 19px;
   vertical-align: middle;
-  }};
 `;
 
 const Navigation = props => {
@@ -41,25 +40,25 @@ const Navigation = props => {
       <Menu>
         <MenuItem>
           <Link href="#">
-            <Icon src={HomeIcon} />
+            <Icon src={homeIcon} />
             <MenuItemText>Home</MenuItemText>
           </Link>
         </MenuItem>
         <MenuItem>
           <Link href="#">
-            <Icon src={MomentsIcon} />
+            <Icon src={momentsIcon} />
             <MenuItemText>Moments</MenuItemText>
           </Link>
         </MenuItem>
         <MenuItem>
           <Link href="#">
-            <Icon src={NotificationsIcon} />
+            <Icon src={notificationsIcon} />
             <MenuItemText>Notifications</MenuItemText>
           </Link>
         </MenuItem>
         <MenuItem>
           <Link href="#">
-            <Icon src={MessagesIcon} />
+            <Icon src={messagesIcon} />
             <MenuItemText>Messages</MenuItemText>
           </Link>
         </MenuItem>
