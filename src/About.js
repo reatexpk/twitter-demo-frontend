33f39c5b87a7ef47ec2f5b6cca887b36a81a55e0
reatex <reatexpk1@gmail.com>
2018-06-20 07:49:10 +0500

    Added About block

--- src/About.js
+++ src/About.js
@@ -1,8 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledAbout = styled.p`
+  margin-top: 23px;
+  line-height: 18px;
+  font-size: 13px;
+  color: #718290;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #718290;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <StyledAbout>
+      © 2018 Twitter <Link href="">About</Link> <Link href="">Help Center</Link>{" "}
+      <Link href="">Terms</Link> <Link href="">Privacy policy</Link>{" "}
+      <Link href="">Cookies</Link> <Link href="">Ads info</Link>
+    </StyledAbout>
+  );
 };
 
 export default About;
