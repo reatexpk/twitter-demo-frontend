0ac8308e176e48e5de8c649f0720c7d448b3c9bc
reatex <reatexpk1@gmail.com>
2018-06-20 07:39:03 +0500

    Added Trends

---
+++ src/Trend.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTrend = styled.div`
+  margin: 0 16px;
+  padding: 5px 0;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  line-height: 20px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #1da1f2;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Text = styled.p`
+  margin: 0;
+  color: #697787;
+  line-height: 20px;
+  font-size: 15px;
+`;
+
+const Trend = props => {
+  return (
+    <StyledTrend>
+      <Link href="">{props.title}</Link>
+      {props.text && <Text>{props.text}</Text>}
+    </StyledTrend>
+  );
+};
+
+export default Trend;
--- src/Trends.js
+++ src/Trends.js
@@ -1,5 +1,63 @@
 import React from "react";
+import styled from "styled-components";
 
-const Trends = () => <div>Trends gonna be here</div>;
+import Trend from "./Trend";
+
+const StyledTrends = styled.div`
+  min-height: 389px;
+  margin-top: 10px;
+  padding-top: 15px;
+  background: #fff;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: baseline;
+  margin-left: 16px;
+  padding-bottom: 4px;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+  font-size: 18px;
+  letter-spacing: -0.3px;
+  color: #000;
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  font-size: 12px;
+  line-height: normal;
+  color: #1da1f2;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Dot = styled.span`
+  margin: 0 6px;
+  color: #66757f;
+`;
+
+const Trends = () => {
+  return (
+    <StyledTrends>
+      <Header>
+        <Title>United Kingdom Trends</Title>
+        <Dot>·</Dot>
+        <StyledLink href="">Change</StyledLink>
+      </Header>
+      <Trend title="#BringYourDogToWorkDay" />
+      <Trend title="#FridayFeeling" text="12.1K Tweets" />
+      <Trend
+        title="#BrexitAnniversary"
+        text="It’s one year since the UK voted to leave the European Union"
+      />
+      <Trend title="HMS Queen Elizabeth" text="1,036 Tweets" />
+      <Trend title="Joe Budden" text="1,036 Tweets" />
+      <Trend title="Trident" text="6,136 Tweets" />
+    </StyledTrends>
+  );
+};
 
 export default Trends;
