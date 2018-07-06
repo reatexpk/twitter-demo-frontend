96202353b16036819ab1dc39110ddf57ffc62876
reatex <reatexpk1@gmail.com>
2018-07-07 02:33:10 +0500

    Refactored code in CoverImage

--- src/CoverImage.js
+++ src/CoverImage.js
@@ -1,13 +1,7 @@
-import React from 'react';
 import styled from 'styled-components';
 
-const CoverImage = ({ src }) => {
-  const StyledCoverImage = styled.div`
-    background: url(${src}) no-repeat;
-    background-size: cover;
-    height: 380px;
-  `;
-  return <StyledCoverImage />;
-};
-
-export default CoverImage;
+export default styled.div`
+  background: url('${({ src }) => src}') no-repeat;
+  background-size: cover;
+  height: 380px;
+`;
